faad082d86ff796401b81fb2120b59dd56e34dc5
binbinq <4688998@qq.com>
2016-10-04 11:59:55 -0700

    1.add common.txt inorder to install some python libs 2.configure mysql in settings.py

--- happyhouse/happyhouse/settings.py
+++ happyhouse/happyhouse/settings.py
@@ -76,11 +76,14 @@ WSGI_APPLICATION = 'happyhouse.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql', # Add 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
+        'NAME': 'csvt',                      # Or path to database file if using sqlite3.
+        'USER': 'root',                      # Not used with sqlite3.
+        'PASSWORD': '123456',                  # Not used with sqlite3.
+        'HOST': '',                      # Set to empty string for localhost. Not used with sqlite3.
+        'PORT': '',                      # Set to empty string for default. Not used with sqlite3.
     }
 }
-
 
 # Internationalization
 # https://docs.djangoproject.com/en/1.8/topics/i18n/
